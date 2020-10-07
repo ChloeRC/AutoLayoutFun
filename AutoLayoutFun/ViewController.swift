ed8ead9317e4fe55da6f0ffebcdf90becdb53bb5
Chloe Crawford <evilbees@comcast.net>
2020-10-07 11:26:48 -0700

    Initial Commit

---
+++ AutoLayoutFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutFun
+//
+//  Created by Chloe Crawford on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
